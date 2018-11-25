29b47572a0af0b5bd8981168c9419ff3cc987d0e
pioui <37812011+pioui@users.noreply.github.com>
2018-11-25 23:52:47 +0200

    dft and inverse dft
    to be used for frequency filtration (?)

---
+++ include/core/transformations/fourier.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "core/transformations/fourier.hpp"
+
+//inverse and normal discrete fourier transformation
+//In order to use it: #include "core/transformations/fourier.hpp"
+
+
+void compute_dft(cv::Mat &in, cv::Mat &complex)
+{
+  cv::Mat padded;                            //expand input image to optimal size
+  int m = getOptimalDFTSize( in.rows );
+  int n = getOptimalDFTSize( in.cols ); // on the border add zero values
+  copyMakeBorder(in, padded, 0, m - in.rows, 0, n - in.cols, cv::BORDER_CONSTANT, Scalar::all(0));
+  cv::Mat planes[] = {cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F)};
+  merge(planes, 2, complex);                  // Add to the expanded another plane with zeros
+  dft(complex, complex, cv::DFT_COMPLEX_OUTPUT);  // fourier transform
+}
+
+void compute_idft(cv::Mat &out, cv::Mat &complex)
+{
+  idft(complex, out);
+  cv::Mat planes[] = {
+      cv::Mat::zeros(complex.size(), CV_32F),
+      cv::Mat::zeros(complex.size(), CV_32F)
+  };
+  split(out, planes); // planes[0] = Re(DFT(I)), planes[1] = Im(DFT(I))
+  magnitude(planes[0], planes[1], out); // sqrt(Re(DFT(I))^2 + Im(DFT(I))^2)
+  normalize(out, out, 0, 1, cv::NORM_MINMAX);
+}
